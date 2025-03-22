5880c6d267305eb3797d717cda4ecff3afa3fb05
Rodrigo Cordeiro <rod.mcordeiro@gmail.com>
2025-03-22 10:54:28 -0300

    Criação dos packages e das classes | Edição do Aplication.properties

---
+++ api_marcacao_consultas/src/main/java/com/fiap/ecr/api_marcacao_consultas/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package main.java.com.fiap.ecr.api_marcacao_consultas.dto;
+
+import lombok.Data;
+
+@Data // Gera automaticamente getters, setters, toString, etc
+public class LoginRequest {
+    private String email;
+    private String senha;
+}
